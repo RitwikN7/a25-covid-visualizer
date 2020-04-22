01bd7cf9f74c3a5d4cd9059bbfc378244a58ed8f
Timothy Bostanche <tbostanche@wisc.ed>
2020-04-22 18:55:44 -0500

    This is hard

---
+++ src/data_parsing/CountryManagerTester.java
@@ -0,0 +1,8 @@
+package data_parsing;
+
+public class CountryManagerTester {
+  
+  public static void main(String[] args) {
+    CountryManager manager = new CountryManager("timeseries.json");
+  }
+}
